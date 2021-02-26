a7259910c86c2007f3b66aa9b9199f641a0dbd6c
nasehbadalov <nasehbadalov@gmail.com>
2021-02-26 23:05:52 +0400

    added sass and styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Components
 import App from './App';
+// Styles
+import 'antd/dist/antd.css';
+import "assets/style.scss";
 
 ReactDOM.render(
   <React.StrictMode>
